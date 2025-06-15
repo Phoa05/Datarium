1a9c6e556a596489760297278868cfc4d1ace176
Pedro Henrique <pedro.phoa1405@gmail.com>
2025-06-15 01:33:28 -0300

    feat: adicionando configurações da aplicacao

---
+++ assets-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO asset (name, ticker, type, risk_level, expected_return, liquidity_days)
+VALUES
+    ('Tesla Inc', 'TSLA', 'STOCK', 8.5, 15.0, 1),
+    ('Brazil Treasury Bond', 'BZTB11', 'BOND', 2.0, 6.5, 30),
+    ('Bitcoin', 'BTC', 'CRYPTO', 9.8, 30.0, 1);
