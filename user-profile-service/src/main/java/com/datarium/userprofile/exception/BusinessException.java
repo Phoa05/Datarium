153dad00fe031b0cc2b1310ee8cdffc0e113f2a8
Pedro Henrique <pedro.phoa1405@gmail.com>
2025-06-15 16:33:51 -0300

    feat: criando exceptions personalizadas

---
+++ user-profile-service/src/main/java/com/datarium/userprofile/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.datarium.userprofile.exception;
+
+public class BusinessException  extends RuntimeException{
+    public BusinessException (String message){
+        super(message);
+    }
+}
